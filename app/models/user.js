ff52caa091381a71190264e597c0c235aed1f44d
karizo502 <karizo502@gmail.com>
2017-06-26 00:25:27 +0700

    change

--- app/models/user.js
+++ app/models/user.js
@@ -1,44 +1,54 @@
-// load the things we need
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+var bcrypt = require('bcryptjs');
 
-// define the schema for our user model
-var userSchema = mongoose.Schema({
-
-    local            : {
-        email        : String,
-        password     : String
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
-
+// Schema defines how the user's data will be stored in MongoDB
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['Client', 'Manager', 'Admin'],
+    default: 'Client'
+  }
 });
 
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// Saves the user's password hashed (plain text password storage is not good)
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+// Create method to compare password input to password saved in database
+UserSchema.methods.comparePassword = function(pw, cb) {
+  bcrypt.compare(pw, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
 };
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
-// create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,47 +1,93 @@
-// server.js
-
-// set up ======================================================================
-// get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 3000;
+// Include our packages in our main server file
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var config = require('./config/config');
+var User = require('./app/models/user');
+var jwt = require('jsonwebtoken');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+// Use body-parser to get POST requests for API use
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var configDB = require('./config/database.js');
+// Log requests to console
+app.use(morgan('dev'));
 
-// configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
-
-require('./config/passport')(passport); // pass passport for configuration
-
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('view engine', 'ejs'); // set up ejs for templating
-
-// required for passport
-app.use(session({
-    secret: 'ilovescotchscotchyscotchscotch', // session secret
-    resave: true,
-    saveUninitialized: true
-}));
+// Initialize passport for use
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
 
-// routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+// Home route. We'll end up changing this to our main front end index later.
+app.get('/', function(req, res) {
+  res.send('Relax. We will put the home page here later.');
+});
 
-// launch ======================================================================
+// Connect to database
+mongoose.connect(config.database);
+
+// Bring in defined Passport Strategy
+require('./config/passport')(passport);
+
+// Create API group routes
+var apiRoutes = express.Router();
+
+// Register new users
+apiRoutes.post('/register', function(req, res) {
+  if(!req.body.email || !req.body.password) {
+    res.json({ success: false, message: 'Please enter email and password.' });
+  } else {
+    var newUser = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    // Attempt to save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({ success: false, message: 'That email address already exists.'});
+      }
+      res.json({ success: true, message: 'Successfully created new user.' });
+    });
+  }
+});
+
+// Authenticate the user and get a JSON Web Token to include in the header of future requests.
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({ success: false, message: 'Authentication failed. User not found.' });
+    } else {
+      // Check if password matches
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 10080 // in seconds
+          });
+          res.json({ success: true, token: 'JWT ' + token });
+        } else {
+          res.send({ success: false, message: 'Authentication failed. Passwords did not match.' });
+        }
+      });
+    }
+  });
+});
+
+// Protect dashboard route with JWT
+apiRoutes.get('/dashboard', passport.authenticate('jwt', { session: false }), function(req, res) {
+  res.send('It worked! User id is: ' + req.user._id + '.');
+});
+
+// Set url for API group routes
+app.use('/api', apiRoutes);
+
+// Start the server
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('Your server is running on port ' + port + '.');
